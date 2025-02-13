65fd60b463f13141020e580f46f0eb03c4653cb4
Antonio Zanon <antonio.zanon99@gmail.com>
2025-02-13 17:24:24 -0300

    Iniciando código Javascript, arrays dos verbos

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+const listaDeVerbos = ["To be", "To have", "To do", "To say", "To go", "To get", "To make", "To know", "To think", "To take"];
+const litaTraducoesVerbos = {
+    "To be": ["ser", "estar"],
+    "To have": ["ter", "possuir"],
+    "To do": ["fazer", "executar"],
+    "To say": ["dizer", "falar"],
+    "To go": ["ir"],
+    "To get": ["obter", "conseguir", "receber"],
+    "To make": ["fazer", "criar", "produzir"],
+    "To know": ["saber", "conhecer"],
+    "To think": ["pensar", "achar"],
+    "To take": ["pegar", "levar"]
+};
+
+let verbosDisponiveis = [...listaDeVerbos];
 
 
 
 
 function startNivel1(){
     let fase = 1;
-    document.querySelector('h2').innerHTML = `Fase ${fase}/10`;
     document.querySelector('.container__nivel__conteudo').innerHTML = `
-    <p>Verbo: </p><h2>To Be</h2>
-    <p>Tradução: </p> <input type="text" id="resposta" placeholder="Digite a resposta">
- `;
+    <h2>Fase ${fase}/10</h2>
+    <h1 class="destaque">To Be</h1>
+    <p>Tradução: </p> 
+    <input type="text" id="resposta" placeholder="Digite a resposta">
+    <br>
+    <br>
+    <br>
+    <button onclick="checarVerbo()" class="btn__checarVerbo">Check</button>
+    `;
 }
