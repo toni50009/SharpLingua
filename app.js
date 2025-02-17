fc136587fd58ed554c0286e7ceca3a5d5daadcf8
Antonio Zanon <antonio.zanon99@gmail.com>
2025-02-17 16:56:24 -0300

    Esqueleto Nível 1 Pronto

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const listaDeVerbos = ["To be", "To have", "To do", "To say", "To go", "To get", "To make", "To know", "To think", "To take"];
-const litaTraducoesVerbos = {
+
+// Arrays de verbos e traduções
+
+const listaDeVerbos = ["To be", "To have", "To do", "To say", "To go", "To get", "To make", "To know", "To think", "To take"
+    ,"To see", "To come", "To want", "To look", "To use", "To find", "To give", "To tell", "To work", "To call",
+];
+const listaTraducoesVerbos = {
     "To be": ["ser", "estar"],
     "To have": ["ter", "possuir"],
     "To do": ["fazer", "executar"],
@@ -9,24 +14,85 @@ const litaTraducoesVerbos = {
     "To make": ["fazer", "criar", "produzir"],
     "To know": ["saber", "conhecer"],
     "To think": ["pensar", "achar"],
-    "To take": ["pegar", "levar"]
+    "To take": ["pegar", "levar"],
+    "To see": ["ver"],
+    "To come": ["vir"],
+    "To want": ["querer"],
+    "To look": ["olhar", "parecer"],
+    "To use": ["usar", "utilizar"],
+    "To find": ["encontrar", "achar"],
+    "To give": ["dar"],
+    "To tell": ["contar", "dizer"],
+    "To work": ["trabalhar", "funcionar"],
+    "To call": ["chamar", "ligar"],
 };
 
 let verbosDisponiveis = [...listaDeVerbos];
-
+let verboAtual = "";
+let fase = 1;
 
 
 
 function startNivel1(){
-    let fase = 1;
+    if(verbosDisponiveis.length === 0){
+        alert('Parabéns, você descobriu todos os verbos!');
+        verbosDisponiveis = [...listaDeVerbos];
+        return;
+    }
+
+    verboAtual = escolherVerboAleatorio();
+
     document.querySelector('.container__nivel__conteudo').innerHTML = `
-    <h2>Fase ${fase}/10</h2>
-    <h1 class="destaque">To Be</h1>
-    <p>Tradução: </p> 
-    <input type="text" id="resposta" placeholder="Digite a resposta">
+    <h2>Fase ${fase}/20</h2>
     <br>
-    <br>
-    <br>
-    <button onclick="checarVerbo()" class="btn__checarVerbo">Check</button>
-    `;
+    <h1 class="destaque">${verboAtual}</h1>
+        <section class="container__nivel__conteudo__inputs">
+            <p>Tradução: </p> 
+            <input type="text" id="campoTentativa" placeholder="Digite a resposta">
+        </section>
+        <br>
+        <br>
+        <br>
+        <button onclick="checarVerbo()" class="btn__checarVerbo">Check</button>
+        `;
+
+    document.getElementById("campoTentativa").focus();
+
+    // Para chamar o botão apertando a tecla Enter
+    document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            checarVerbo();
+        }
+    });
+
+}
+
+
+function escolherVerboAleatorio(){
+    const indice = Math.floor(Math.random() * verbosDisponiveis.length);
+    const verboEscolhido = verbosDisponiveis[indice];
+    verbosDisponiveis.splice(indice, 1); // Remove o verbo escolhido da lista de verbos disponíveis
+    return verboEscolhido;
+}
+
+
+function checarVerbo(){
+    const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
+    const respostasAceitas = listaTraducoesVerbos[verboAtual].map(resposta => resposta.toLowerCase());
+    
+    // coletar dados do HTML
+    const resultadoDiv = document.querySelector(".container__nivel__conteudo__resultados");
+    const resultadoTexto = document.getElementById("resultado");
+    const respostasTexto = document.getElementById("respostasCorretas");
+
+    // Verificar se a resposta do usuário está correta
+    resultadoDiv.classList.remove("invisivel");
+
+    if(respostasAceitas.includes(respostaUsuario)){
+        resultadoTexto.innerText = "Correto!";
+        resultadoDiv.classList.remove("erro");
+    } else {
+        resultadoTexto.innerText = "Incorreto!";
+        resultadoDiv.classList.add("erro");
+    }
 }
