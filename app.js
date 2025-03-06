f2d23d5fa4211398f34759e028075a9b274d7d58
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-06 16:50:28 -0300

    TERMINAR EM CASA HOJE

--- app.js
+++ app.js
@@ -85,13 +85,12 @@ let fase = 1;
 //Nivel 2
 let phrasalVerbsDisponiveis = [...listaPharsalVerbs];
 let phrasalVerbAtual = "";
-let fasePhrasal = 1;
 
 
 
 function startNivel1(){
 
-    verboAtual = escolherVerboAleatorio();
+    verboAtual = escolherAleatorio(verbosDisponiveis);
 
     mensagemInicial("checarVerbo",verboAtual);
 
@@ -107,7 +106,7 @@ function startNivel1(){
 
 function startNivel2(){
 
-    phrasalVerbAtual = escolherPhrasalVerbAleatorio();
+    phrasalVerbAtual = escolherAleatorio(phrasalVerbsDisponiveis);
 
     mensagemInicial("checarPhrasalVerb",phrasalVerbAtual);
 
@@ -121,23 +120,56 @@ function startNivel2(){
 
 
 
-function escolherVerboAleatorio(){
-    const indice = Math.floor(Math.random() * verbosDisponiveis.length);
-    const verboEscolhido = verbosDisponiveis[indice];
-    verbosDisponiveis.splice(indice, 1); // Remove o verbo escolhido da lista de verbos disponíveis
-    return verboEscolhido;
+function escolherAleatorio(lista){
+    const indice = Math.floor(Math.random() * lista.length);
+    const escolhido = lista[indice];
+    lista.splice(indice, 1); // Remove o verbo escolhido da lista de verbos disponíveis
+    return escolhido;
 }
 
 
-function escolherPhrasalVerbAleatorio(){
-    const indice = Math.floor(Math.random() * phrasalVerbsDisponiveis.length);
-    const phrasalVerbEscolhido = phrasalVerbsDisponiveis[indice];
-    phrasalVerbsDisponiveis.splice(indice, 1); // Remove o verbo escolhido da lista de verbos disponíveis
-    return phrasalVerbEscolhido;
+function checarResposta(verbo, listaTraducao){
+    const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
+    const respostasAceitas = listaTraducao[verbo].map(resposta => resposta.toLowerCase());
+    
+    // coletar dados do HTML
+    const resultadoDiv = document.querySelector(".container__nivel__conteudo__resultados");
+    const campoBotao = document.querySelector(".container__nivel__conteudo__botao");
+    const resultadoTexto = document.getElementById("resultado");
+    const respostasTexto = document.getElementById("respostasCorretas");
+
+    // Verificar se a resposta do usuário está correta
+    resultadoDiv.classList.remove("invisivel");
+
+    if(respostasAceitas.includes(respostaUsuario)){
+        resultadoTexto.innerHTML = "Correto!";
+        respostasTexto.innerHTML = `Traduções aceitas: ${respostasAceitas.join(", ")}. 
+        <br>
+        <br>
+        Tecle "Enter" ou clique no botão para prosseguir.`;
+        resultadoDiv.classList.remove("erro");
+        campoBotao.innerHTML = `
+        <button onclick="proximoPasso()" class="btn__proximoVerbo">Próximo Verbo!</button>
+        `;
+        //otimizar com enter
+        document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+               proximoVerbo();
+            }
+        });
+
+    } else {
+        resultadoTexto.innerText = "Incorreto!";
+        resultadoDiv.classList.add("erro");
+        respostasTexto.textContent = "";
+        document.getElementById("campoTentativa").value = "";
+    }
+
 }
 
 
-function checarVerbo(listaRef, verbo){
+
+function checarVerbo(){
     const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
     const respostasAceitas = listaTraducoesVerbos[verboAtual].map(resposta => resposta.toLowerCase());
     
@@ -218,7 +250,7 @@ function checarPhrasalVerb(){
 
 
 function proximoVerbo(){
-    if(fase === 20){
+    if(fase === 5){
         document.querySelector(".container__nivel").innerHTML = `
         <div class="container__nivel__conteudo__final">
         Parabéns! Você completou todas as fases!
@@ -241,7 +273,7 @@ function proximoVerbo(){
 
 
 function proximoPhrasalVerb(){
-    if(fase === 20){
+    if(fase === 5){
         document.querySelector(".container__nivel").innerHTML = `
         <div class="container__nivel__conteudo__final">
         Parabéns! Você completou todas as fases!
@@ -276,7 +308,7 @@ function mensagemInicial(target,opcao){
         <br>
         <br>
         <div class="container__nivel__conteudo__botao">
-        <button onclick="${target}" class="btn__checarVerbo">Check</button>
+        <button onclick="${target}()" class="btn__checarVerbo">Check</button>
         </div>
         `;
 
