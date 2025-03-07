27e0a9444e9e022c3d7b99fbb5a5c56b5a296d5a
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-07 00:33:43 -0300

    Corrigir Bug Palavra Tentativa ( Não está funcionando corretamente).

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ const listaPharsalVerbsTraducoes = {
 
 
 //Universal
-let tentativas = 0;
+let tentativas = 3;
 
 //Nivel 1
 let verbosDisponiveis = [...listaDeVerbos];
@@ -88,33 +88,22 @@ let phrasalVerbAtual = "";
 
 
 
-function startNivel1(){
+function iniciarNivel(tipo){
+    console.log(`Tentativas desde o começo: ${tentativas}`);
+    console.log(tipo);
+    if(tipo === "verbo"){
+        verboAtual = escolherAleatorio(verbosDisponiveis);
+        mensagemInicial(`checarResposta(${verboAtual}, ${listaTraducoesVerbos})`, verboAtual);
+    }else{
+        phrasalVerbAtual = escolherAleatorio(phrasalVerbsDisponiveis);
+        mensagemInicial(`checarResposta(${phrasalVerbAtual}, ${listaPharsalVerbsTraducoes})`, phrasalVerbAtual);
+    }
 
-    verboAtual = escolherAleatorio(verbosDisponiveis);
-
-    mensagemInicial("checarVerbo",verboAtual);
-
-    document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            checarVerbo();
-        }
-    });
-
-
-}
-
-
-function startNivel2(){
-
-    phrasalVerbAtual = escolherAleatorio(phrasalVerbsDisponiveis);
-
-    mensagemInicial("checarPhrasalVerb",phrasalVerbAtual);
-
-    document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            checarPhrasalVerb();
-        }
-    });
+        document.getElementById("campoTentativa").addEventListener("keypress", function(event){
+            if(event.key === "Enter"){
+                tipo === "verbo" ? checarResposta(verboAtual, listaTraducoesVerbos) : checarResposta(phrasalVerbAtual, listaPharsalVerbsTraducoes);
+            }
+        });
 
 }
 
@@ -123,12 +112,15 @@ function startNivel2(){
 function escolherAleatorio(lista){
     const indice = Math.floor(Math.random() * lista.length);
     const escolhido = lista[indice];
+    console.log(`Verbo/Phrasal escolhido: ${escolhido}`);
     lista.splice(indice, 1); // Remove o verbo escolhido da lista de verbos disponíveis
+    console.log(` lista de verbos/Phrasal: ${lista}`);
     return escolhido;
 }
 
 
 function checarResposta(verbo, listaTraducao){
+    console.log(`Tentativas antes da checagem: ${tentativas}`);
     const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
     const respostasAceitas = listaTraducao[verbo].map(resposta => resposta.toLowerCase());
     
@@ -149,107 +141,47 @@ function checarResposta(verbo, listaTraducao){
         Tecle "Enter" ou clique no botão para prosseguir.`;
         resultadoDiv.classList.remove("erro");
         campoBotao.innerHTML = `
-        <button onclick="proximoPasso()" class="btn__proximoVerbo">Próximo Verbo!</button>
+        <button onclick="proximoPasso()" class="btn__proximoVerbo">Próxima fase!</button>
         `;
+        console.log(`Tentativas caso acerto: ${tentativas}`);
         //otimizar com enter
         document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
             if (event.key === "Enter") {
-               proximoVerbo();
+               proximoPasso();
             }
         });
 
     } else {
-        resultadoTexto.innerText = "Incorreto!";
+        tentativas --;
+        console.log(`Tentativas caso erro: ${tentativas}`);
+        if(tentativas === 0) {
+            console.log(`Perdeu, tentativas: ${tentativas}`);
+            document.querySelector(".container__nivel").innerHTML = `
+            <div class="container__nivel__conteudo__final">
+            Sem mais tentativas restantes! 
+            A resposta era:${respostasAceitas}
+            <br>
+            <a href="seletorniveis.html" class="link__menu" style="text-decoration:underline">Clique aqui para voltar ao menu principal</a>
+            </div>
+            `;
+            return;
+        }
+        resultadoTexto.innerHTML = `Incorreto! ${tentativas} ${palavraTentativa} restante.`;
         resultadoDiv.classList.add("erro");
         respostasTexto.textContent = "";
         document.getElementById("campoTentativa").value = "";
+
+
     }
 
 }
 
+// para aparecer a palavra "tentativa" corretamente
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
 
 
-function checarVerbo(){
-    const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
-    const respostasAceitas = listaTraducoesVerbos[verboAtual].map(resposta => resposta.toLowerCase());
-    
-    // coletar dados do HTML
-    const resultadoDiv = document.querySelector(".container__nivel__conteudo__resultados");
-    const campoBotao = document.querySelector(".container__nivel__conteudo__botao");
-    const resultadoTexto = document.getElementById("resultado");
-    const respostasTexto = document.getElementById("respostasCorretas");
 
-    // Verificar se a resposta do usuário está correta
-    resultadoDiv.classList.remove("invisivel");
-
-    if(respostasAceitas.includes(respostaUsuario)){
-        resultadoTexto.innerHTML = "Correto!";
-        respostasTexto.innerHTML = `Traduções aceitas: ${respostasAceitas.join(", ")}. 
-        <br>
-        <br>
-        Tecle "Enter" ou clique no botão para prosseguir.`;
-        resultadoDiv.classList.remove("erro");
-        campoBotao.innerHTML = `
-        <button onclick="proximoVerbo()" class="btn__proximoVerbo">Próximo Verbo!</button>
-        `;
-        //otimizar com enter
-        document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
-            if (event.key === "Enter") {
-               proximoVerbo();
-            }
-        });
-
-    } else {
-        resultadoTexto.innerText = "Incorreto!";
-        resultadoDiv.classList.add("erro");
-        respostasTexto.textContent = "";
-        document.getElementById("campoTentativa").value = "";
-    }
-
-}
-
-function checarPhrasalVerb(){
-    const respostaUsuario = document.getElementById("campoTentativa").value.trim().toLowerCase();
-    const respostasAceitas = listaPharsalVerbsTraducoes[phrasalVerbAtual].map(resposta => resposta.toLowerCase());
-    
-    // coletar dados do HTML
-    const resultadoDiv = document.querySelector(".container__nivel__conteudo__resultados");
-    const campoBotao = document.querySelector(".container__nivel__conteudo__botao");
-    const resultadoTexto = document.getElementById("resultado");
-    const respostasTexto = document.getElementById("respostasCorretas");
-
-    // Verificar se a resposta do usuário está correta
-    resultadoDiv.classList.remove("invisivel");
-
-    if(respostasAceitas.includes(respostaUsuario)){
-        resultadoTexto.innerHTML = "Correto!";
-        respostasTexto.innerHTML = `Traduções aceitas: ${respostasAceitas.join(", ")}. 
-        <br>
-        <br>
-        Tecle "Enter" ou clique no botão para prosseguir.`;
-        resultadoDiv.classList.remove("erro");
-        campoBotao.innerHTML = `
-        <button onclick="proximoVerbo()" class="btn__proximoVerbo">Próximo Phrasal Verb!</button>
-        `;
-        //otimizar com enter
-        document.getElementById("campoTentativa").addEventListener("keypress", function(event) {
-            if (event.key === "Enter") {
-               proximoPhrasalVerb();
-            }
-        });
-
-    } else {
-        resultadoTexto.innerText = "Incorreto!";
-        resultadoDiv.classList.add("erro");
-        respostasTexto.textContent = "";
-        document.getElementById("campoTentativa").value = "";
-    }
-
-
-}
-
-
-function proximoVerbo(){
+function proximoPasso(){
     if(fase === 5){
         document.querySelector(".container__nivel").innerHTML = `
         <div class="container__nivel__conteudo__final">
@@ -259,28 +191,6 @@ function proximoVerbo(){
         </div>
         `;
         verbosDisponiveis = [...listaDeVerbos];
-        fase = 1;
-        return;
-    }
-
-    fase++;
-    document.getElementById("campoTentativa").value = "";
-    document.getElementById("campoTentativa").focus();
-    document.querySelector(".container__nivel__conteudo__resultados").classList.add("invisivel");
-    startNivel1();
-}
-
-
-
-function proximoPhrasalVerb(){
-    if(fase === 5){
-        document.querySelector(".container__nivel").innerHTML = `
-        <div class="container__nivel__conteudo__final">
-        Parabéns! Você completou todas as fases!
-        <br>
-        <a href="seletorniveis.html" class="link__menu" style="text-decoration:underline">Clique aqui para voltar ao menu principal</a>
-        </div>
-        `;
         phrasalVerbsDisponiveis = [...listaPharsalVerbs];
         fase = 1;
         return;
@@ -290,7 +200,13 @@ function proximoPhrasalVerb(){
     document.getElementById("campoTentativa").value = "";
     document.getElementById("campoTentativa").focus();
     document.querySelector(".container__nivel__conteudo__resultados").classList.add("invisivel");
-    startNivel2();
+    
+    if(verboAtual){
+        iniciarNivel("verbo");
+    }else{
+        iniciarNivel("phrasal");
+    }
+
 }
 
 
