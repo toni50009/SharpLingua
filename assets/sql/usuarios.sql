da65695e6c41f59ec17be3cca0e2d83a3823635b
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-20 16:59:23 -0300

    Alteração arquivo SQL , inclusão pontos

--- assets/sql/usuarios.sql
+++ assets/sql/usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 18/03/2025 às 22:52
+-- Tempo de geração: 20/03/2025 às 19:16
 -- Versão do servidor: 10.11.10-MariaDB
 -- Versão do PHP: 7.2.34
 
@@ -28,11 +28,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(10) NOT NULL UNIQUE,
-  `senha` varchar(10) NOT NULL
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `senha` varchar(10) NOT NULL,
+  `pontos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `senha`, `pontos`) VALUES
+(2, 'antonio', 'antonio', 0),
+(3, 'antoniozan', 'antoniozan', 0),
+(4, 'tatyy', '123456', 0),
+(5, 'lucas', '1234', 0),
+(8, 'toni200', '123', 0),
+(9, 'toni500', 'Dementador', 0),
+(10, '1234', '1234', 0),
+(11, 'amtomio', 'zamom', 0),
+(12, 'teste123', 'teste', 0),
+(13, 'JAO', 'SEMBRACO', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -41,6 +57,8 @@ CREATE TABLE `usuarios` (
 --
 -- Índices de tabela `usuarios`
 --
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -50,7 +68,7 @@ CREATE TABLE `usuarios` (
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
