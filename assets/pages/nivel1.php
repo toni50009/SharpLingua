36ce5a3d11b1d8465d213826ba24888360dca3d6
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-20 22:29:41 -0300

    Correção mensagens erros

--- assets/pages/nivel1.php
+++ assets/pages/nivel1.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     require __DIR__ . '/assets/php/config.php';
 
     session_start();
@@ -7,9 +11,7 @@
         exit();
     }
 
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 ?>
 
