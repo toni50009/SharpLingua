e62e08f5cac4d1c5b9967c4c59beccbe0b5caf97
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-21 00:58:08 -0300

    Correção Cadastro, inclusão de nome

--- assets/php/cadastro.php
+++ assets/php/cadastro.php
@@ -6,11 +6,12 @@ require __DIR__ . '/config.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     
+    $nome = $_POST['new_nome'];
     $username = $_POST['new_username'];
     $senha = $_POST['new_senha'];
 
  // VERIFICAR SE CAMPO ESTA VAZIO
-     if($username == " " || empty($username) || $senha == " " || empty($senha)) {
+     if( empty($nome) || $nome == " " || $username == " " || empty($username) || $senha == " " || empty($senha)) {
           echo "<script>alert('Preencha todos os campos!');</script>";
           echo "<script>window.location.href = '/assets/pages/cadastro.html';</script>";
          exit();
@@ -42,14 +43,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     //INSERIR NOVO USUARIO
-    $sql = "INSERT INTO usuarios (username,senha) VALUES (?, ?)";
+    $sql = "INSERT INTO usuarios (nome,username,senha) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
 
     if(!$stmt) {
         die("Erro ao inserir usuário: " . $conn->error);
     }
 
-    $stmt->bind_param("ss", $username, $senha);
+    $stmt->bind_param("sss", $username, $senha,$nome);
     if($stmt->execute()){
         echo "<script>alert('Usuário cadastrado com sucesso!');</script>";
         echo "<script>window.location.href = '/index.html';</script>";
