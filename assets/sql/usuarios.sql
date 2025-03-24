429755aa7b2ac369c61ba2732b7bf26620f60094
Antonio Zanon <antonio.zanon99@gmail.com>
2025-03-24 01:17:57 -0300

    Corrigindo direto no SQL

--- assets/sql/usuarios.sql
+++ assets/sql/usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 21/03/2025 às 03:42
+-- Tempo de geração: 24/03/2025 às 04:18
 -- Versão do servidor: 10.11.10-MariaDB
 -- Versão do PHP: 7.2.34
 
@@ -29,11 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL UNIQUE,
-  `senha` varchar(255) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `senha` varchar(100) DEFAULT NULL,
   `pontos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `username`, `senha`, `pontos`) VALUES
+(33, 'Antonio Zanon', 'toni500', '1234', 0),
+(34, '1234', 'antoniozan', 'antonio', 0),
+(35, '12345', 'antoniozan', '123', 0),
+(36, 'awdjaiwdo', 'antoniozan', 'awdawd', 0),
+(37, 'awdad', 'antoniozan', 'zawd', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -53,7 +64,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
